40764492a73a0e248bb0d8891614e702d11817f2
Manan Shah <manan@flydata.com>
2021-05-22 13:59:55 +0530

    move to different file for function and runner

--- epoch.go
+++ epoch.go
@@ -1,27 +1,10 @@
-package epoch
+package main
 
 import (
 	"fmt"
-	"time"
+	"github.com/killerdejavu/epoch/getter"
 )
 
-func GetCurrentTime() int64 {
-	now := time.Now()
-	secs := now.Unix()
-	fmt.Println(secs)
-	//nanos := now.UnixNano()
-	////
-	////// Note that there is no `UnixMillis`, so to get the
-	////// milliseconds since epoch you'll need to manually
-	////// divide from nanoseconds.
-	//millis := nanos / 1000000
-	////
-	//fmt.Println(millis)
-	//fmt.Println(nanos)
-	//
-	//// You can also convert integer seconds or nanoseconds
-	//// since the epoch into the corresponding `time`.
-	//fmt.Println(time.Unix(secs, 0))
-	//fmt.Println(time.Unix(0, nanos))
-	return secs
+func main() {
+	fmt.Println(getter.GetCurrentTime())
 }
