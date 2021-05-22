366fc0b5d548161499a158649226d7ed428343bf
Manan Shah <manan@flydata.com>
2021-05-22 13:35:41 +0530

    prints current unix time

---
+++ epoch.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	now := time.Now()
+	secs := now.Unix()
+	fmt.Println(secs)
+	//nanos := now.UnixNano()
+	//
+	//// Note that there is no `UnixMillis`, so to get the
+	//// milliseconds since epoch you'll need to manually
+	//// divide from nanoseconds.
+	//millis := nanos / 1000000
+	//
+	//fmt.Println(millis)
+	//fmt.Println(nanos)
+	//
+	//// You can also convert integer seconds or nanoseconds
+	//// since the epoch into the corresponding `time`.
+	//fmt.Println(time.Unix(secs, 0))
+	//fmt.Println(time.Unix(0, nanos))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/killerdejavu/epoch
+
+go 1.16
