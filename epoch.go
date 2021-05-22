893236dafec15244529ab1d104ceb5d030889a20
Manan Shah <manan@flydata.com>
2021-05-22 13:48:07 +0530

    make it a module function

--- epoch.go
+++ epoch.go
@@ -1,26 +1,27 @@
-package main
+package epoch
 
 import (
 	"fmt"
 	"time"
 )
 
-func main() {
+func GetCurrentTime() int64 {
 	now := time.Now()
 	secs := now.Unix()
 	fmt.Println(secs)
-	nanos := now.UnixNano()
-	//
-	//// Note that there is no `UnixMillis`, so to get the
-	//// milliseconds since epoch you'll need to manually
-	//// divide from nanoseconds.
-	millis := nanos / 1000000
-	//
-	fmt.Println(millis)
-	fmt.Println(nanos)
+	//nanos := now.UnixNano()
+	////
+	////// Note that there is no `UnixMillis`, so to get the
+	////// milliseconds since epoch you'll need to manually
+	////// divide from nanoseconds.
+	//millis := nanos / 1000000
+	////
+	//fmt.Println(millis)
+	//fmt.Println(nanos)
 	//
 	//// You can also convert integer seconds or nanoseconds
 	//// since the epoch into the corresponding `time`.
 	//fmt.Println(time.Unix(secs, 0))
 	//fmt.Println(time.Unix(0, nanos))
+	return secs
 }
